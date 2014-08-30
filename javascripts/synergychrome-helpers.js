9d2ce948b107ac0513a941c0230fb7513fe1f7f4
mdvanes <es@mdworld.nl>
2014-08-30 21:54:11 +0200

    generator for automatically redirecting bookmarklet

---
+++ javascripts/synergychrome-helpers.js
@@ -0,0 +1,26 @@
+// (function($) {
+// 	function generate() {
+// 		var $bookmarkletlink = $('#bookmarkletlink');
+// 		var bookmarklet = $bookmarkletlink.attr('href');
+// 		var name = $bookmarkletlink.text();
+// 		var synergyUrl = $('#synergyUrl').val();
+// 		var generatedBookmarklet = bookmarklet.replace(/synergyChrome\.URL=null/,'synergyChrome.URL="' + synergyUrl + '"');
+// 		$('#generatedBookmarkletWrapper > a')
+// 			.attr('href', generatedBookmarklet)
+// 			.text(name);
+// 		$('#defaultInstallation').css('opacity', '0.3');
+// 		$('#generatedBookmarkletWrapper').show();
+// 	}
+
+// 	function init() {
+// 		$('#generatedBookmarkletWrapper').hide();
+
+// 		$('#generateButton').click(function() {
+// 			generate();
+// 		});
+// 	}
+
+// 	$(document).ready(function() {
+// 		init();
+// 	});
+// })(jQuery);
