5e6bf8073aeab0b27096bf89fe1e038c96234521
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2014-08-30 21:55:43 +0200

    attempt for auto redirect

--- js/exports.js
+++ js/exports.js
@@ -1,4 +1,5 @@
 /* globals synergyChrome */
 // Export when wrapped in bookmarklet closure
 window.synergyChrome = {};
+window.synergyChrome.URL = synergyChrome.URL; // Needed for replace script
 window.synergyChrome.info = synergyChrome.info;
