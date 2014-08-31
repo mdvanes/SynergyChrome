cf94e39dc6eddd88fa26aa84f4625d0b99d8596a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2014-08-31 11:51:58 +0200

    updated to 1.1.1

--- javascripts/synergychrome-helpers.js
+++ javascripts/synergychrome-helpers.js
@@ -1,26 +1,26 @@
-// (function($) {
-// 	function generate() {
-// 		var $bookmarkletlink = $('#bookmarkletlink');
-// 		var bookmarklet = $bookmarkletlink.attr('href');
-// 		var name = $bookmarkletlink.text();
-// 		var synergyUrl = $('#synergyUrl').val();
-// 		var generatedBookmarklet = bookmarklet.replace(/synergyChrome\.URL=null/,'synergyChrome.URL="' + synergyUrl + '"');
-// 		$('#generatedBookmarkletWrapper > a')
-// 			.attr('href', generatedBookmarklet)
-// 			.text(name);
-// 		$('#defaultInstallation').css('opacity', '0.3');
-// 		$('#generatedBookmarkletWrapper').show();
-// 	}
+(function($) {
+    function generate() {
+        var $bookmarkletlink = $('#bookmarkletlink');
+        var bookmarklet = $bookmarkletlink.attr('href');
+        var name = $bookmarkletlink.text();
+        var synergyUrl = $('#synergyUrl').val();
+        var generatedBookmarklet = bookmarklet.replace(/synergyChrome\.URL=null/,'synergyChrome.URL="' + synergyUrl + '"');
+        $('#generatedBookmarkletWrapper > a')
+            .attr('href', generatedBookmarklet)
+            .text(name);
+        $('#defaultInstallation').css('opacity', '0.3');
+        $('#generatedBookmarkletWrapper').show();
+    }
 
-// 	function init() {
-// 		$('#generatedBookmarkletWrapper').hide();
+    function init() {
+        $('#generatedBookmarkletWrapper').hide();
 
-// 		$('#generateButton').click(function() {
-// 			generate();
-// 		});
-// 	}
+        $('#generateButton').click(function() {
+            generate();
+        });
+    }
 
-// 	$(document).ready(function() {
-// 		init();
-// 	});
-// })(jQuery);
+    $(document).ready(function() {
+        init();
+    });
+})(jQuery);
